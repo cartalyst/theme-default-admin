a05289b3f0ec927ee17ef70974a0bcf7c0068e3c
Ninja <ninja@ninjaparade.ca>
2014-06-16 08:00:09 -0700

    Update default.blade.php
    add initial scale to viewport meta

--- views/layouts/default.blade.php
+++ views/layouts/default.blade.php
@@ -12,7 +12,7 @@
 			@show
 		</title>
 		<meta name="description" content="@yield('meta-description')">
-		<meta name="viewport" content="width=device-width">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="base_url" content="{{ URL::to('/') }}">
 
 		{{-- Queue assets --}}
