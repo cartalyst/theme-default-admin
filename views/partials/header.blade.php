099c994a84c4c8763488faea19812153e4577e37
drsii <dan.syme@gmail.com>
2014-12-22 23:08:31 -0800

    rough in logged in user menu.

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -4,14 +4,12 @@
 
 		<nav class="navigation navigation--header pull-left">
 
-		<ul class="nav nav-pills">
+			<ul class="nav nav-pills">
 
 				<li>
-
 					<a class="toggle-sidebar" data-tooltip="" data-placement="right" data-title="Sidebar" data-original-title="" title="">
 						<i class="fa fa-bars"></i>
 					</a>
-
 				</li>
 
 			</ul>
@@ -28,6 +26,24 @@
 
 		</nav>
 
+		<nav class="navigation navigation--account pull-right">
+
+			<ul class="nav nav-pills">
+
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ $currentUser->email }} <span class="caret"></span></a>
+
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="{{ URL::toAdmin("users/{$currentUser->id}") }}">Profile</a>
+
+						<li class="divider"></li>
+						<li><a href="{{ URL::to('/logout') }}">Sign Out</a>
+					</ul>
+				</li>
+
+			</ul>
+
+		</nav>
 
 	</div>
 
