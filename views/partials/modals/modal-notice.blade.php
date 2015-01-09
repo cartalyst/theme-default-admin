f12f44cf57e27e26e1ae4026dc7f1eb9ae564a39
drsii <dan.syme@gmail.com>
2015-01-09 13:57:45 -0800

    adding notice modal.

---
+++ views/partials/modals/modal-notice.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="modal-confirm" tabindex="-1" role="dialog" aria-labelledby="model-confirm-label" aria-hidden="true">
+
+	<div class="modal-dialog">
+
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">{{{ trans('common.error') }}}</h4>
+			</div>
+
+			<div class="modal-body">
+
+				<p class="lead text-center"></p>
+
+			</div>
+
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default btn-block" data-dismiss="modal">{{{ trans('action.cancel') }}}</button>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
